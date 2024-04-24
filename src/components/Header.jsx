e4b57084e391cf028307e525588c1b209108f2ad
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-04-24 21:09:22 +0200

    el logo ahora es un enlace a '/'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import SearchBar from './SearchBar'
 import HeaderNav from './HeaderNav'
 function Header() {
   return (
     <header className='flex justify-between p-3 items-center'>
-      <Image src='/logo-dark.svg' alt='logo' width={250} height={50} />
+      <Link href='/'>
+        <Image src='/logo-dark.svg' alt='logo' width={250} height={50} />
+      </Link>
       <SearchBar />
       <HeaderNav />
     </header>
