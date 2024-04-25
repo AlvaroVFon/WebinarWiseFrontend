287a0174a77a0620f037b4d463fd5da7b2515b67
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-04-25 23:12:36 +0200

    comenzado skeleton

--- src/app/home/cursos/page.jsx
+++ src/app/home/cursos/page.jsx
@@ -1,7 +1,12 @@
 import CourseCard from '@/components/CourseCard'
 function CursosPage() {
   return (
-    <div className='grid place-items-center min-h-screen '>
+    <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-10  place-items-center min-h-screen '>
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
       <CourseCard />
     </div>
   )
---
+++ src/components/CourseCardSkeleton.jsx
@@ -0,0 +1,4 @@
+function CourseCardSkeleton(props) {
+  return <div className='w-[285px] h-[414px] p-4 border'></div>
+}
+export default CourseCardSkeleton
