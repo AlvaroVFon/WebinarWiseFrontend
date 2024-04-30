457f02ccab6dd58466b91a846ea998ea2d5535ae
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-04-30 13:35:42 +0200

    perPage=12

--- src/app/home/courses/page.jsx
+++ src/app/home/courses/page.jsx
@@ -4,12 +4,12 @@ import Pagination from '@/components/Pagination'
 import api from '@/lib/api/WebinarWiseApi'
 async function CursosPage({ searchParams }) {
   const { page } = searchParams
-  const url = `/courses?page=${page}`
+  const url = `/courses?page=${page}&perPage=12`
   const courses = await api.getCourses(url)
 
   return (
     <div className=''>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-10  place-items-center min-h-screen p-6'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-y-10  place-items-center min-h-screen p-6'>
         {courses.map((course, index) => (
           <CourseCard key={index} course={course} />
         ))}
