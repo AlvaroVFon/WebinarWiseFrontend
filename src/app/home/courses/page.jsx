feab5f50c7dc0d3db3e0b94ea4e228c6cadff2c0
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-03 16:18:57 +0200

    searchBar fucionando, migrar a react-query

--- src/app/home/courses/page.jsx
+++ src/app/home/courses/page.jsx
@@ -4,20 +4,33 @@ import Pagination from '@/components/Pagination'
 import { Suspense } from 'react'
 import api from '@/lib/api/WebinarWiseApi'
 async function CursosPage({ searchParams }) {
-  const { page } = await searchParams
+  const { page, title } = await searchParams
   const url = `/courses?page=${page}&perPage=12`
   const courses = await api.getCourses(url).catch((error) => error)
+  const filteredCourses = courses.results.filter((course) =>
+    course.name.toLowerCase().includes(title.toLowerCase())
+  )
+
   return (
     <div className=''>
       <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-y-10  place-items-center min-h-screen p-6'>
-        {courses.results?.map((course, index) => (
-          <Suspense key={index} fallback={<CourseCardSkeleton />}>
-            <CourseCard key={index} course={course} />
+        {filteredCourses?.map((course, index) => (
+          <Suspense
+            key={index}
+            fallback={<CourseCardSkeleton />}
+          >
+            <CourseCard
+              key={index}
+              course={course}
+            />
           </Suspense>
         ))}
       </div>
       <div className='flex justify-center'>
-        <Pagination currentPage={page} totalPages={courses.totalPages} />
+        <Pagination
+          currentPage={page}
+          totalPages={courses.totalPages}
+        />
       </div>
     </div>
   )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,24 @@
-async function SearchBar() {
+'use client'
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
+
+function SearchBar() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const router = useRouter()
+  const page = searchParams.get('page') || 1
+
+  const handleChange = (e) => {
+    const title = e.target.value
+    router.push(`${pathname}?page=${page}&title=${title}`)
+  }
   return (
-    <form action='' className=''>
-      <input
-        type='search'
-        placeholder='Search...'
-        className='rounded-lg p-2 placeholder:p-2 bg-bgPrimary border border-muted text-accent placeholder-muted hover:border-accent hover:bg-bgTertiary focus:bg-bgTertiary w-full duration-300'
-      />
-    </form>
+    <input
+      type='search'
+      placeholder='Search...'
+      name='search'
+      onChange={handleChange}
+      className='rounded-lg p-2 placeholder:p-2 bg-bgPrimary border border-muted text-accent placeholder-muted hover:border-accent hover:bg-bgTertiary focus:bg-bgTertiary w-full duration-300'
+    />
   )
 }
 export default SearchBar
