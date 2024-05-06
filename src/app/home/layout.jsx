72947e4781422b709a6a4777adf78820960a753a
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-06 14:22:59 +0200

    header suspense

--- src/app/home/layout.jsx
+++ src/app/home/layout.jsx
@@ -1,8 +1,11 @@
 import Header from '@/components/Header'
+import { Suspense } from 'react'
 function HomeLayout({ children }) {
   return (
     <div className='min-h-screen relative'>
-      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>
       <div className='mt-28 z-0'>{children}</div>
     </div>
   )
