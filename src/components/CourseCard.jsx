ed1a67b4387ebed88f42a85abc67a6de56ff16b6
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-10 13:24:49 +0200

    CourseCard ahora es enlace, rediseño del componente

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,24 +1,29 @@
+'use client'
 import Image from 'next/image'
 import CourseCardButtons from './CourseCardButtons'
 import CourseCardHeader from './CourseCardHeader'
+import { useRouter } from 'next/navigation'
 async function CourseCard({ course, isPurchased, isLiked }) {
-  const { creation_date } = course
-  const date = creation_date.split('T')[0].split('-').reverse().join('-')
+  const router = useRouter()
   return (
     <article className='bg-bgSecondary rounded-xl w-[285px] h-[414px] p-4 border border-muted hover:border-accent flex flex-col justify-between hover:scale-105 duration-300'>
-      <CourseCardHeader course={course} />
-      <div className='flex flex-col items-start gap-2'>
-        <p className='border border-muted text-accentDarker p-1 rounded-md'>
-          #{course.category?.name}
-        </p>
-        <p className='text-xs p-1 text-muted'>{date}</p>
-        <Image
-          src='https://placehold.jp/3d4070/ffffff/300x160.png'
-          alt='Curso de React'
-          width={300}
-          height={160}
-          className='rounded-xl'
-        />
+      <div
+        onClick={() => router.push(`/home/courses/${course.id}`)}
+        className='cursor-pointer'
+      >
+        <CourseCardHeader course={course} />
+        <div className='flex flex-col items-end gap-4'>
+          <p className='border border-muted text-accentDarker p-1 rounded-md'>
+            #{course.category?.name}
+          </p>
+          <Image
+            src='https://placehold.jp/3d4070/ffffff/300x160.png'
+            alt='Curso de React'
+            width={300}
+            height={160}
+            className='rounded-xl'
+          />
+        </div>
       </div>
       <CourseCardButtons
         course={course}
--- src/components/CourseCardHeader.jsx
+++ src/components/CourseCardHeader.jsx
@@ -1,20 +1,18 @@
 import { Roboto } from 'next/font/google'
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700', '900'] })
-import Link from 'next/link'
 function CourseCardHeader({ course }) {
+  const date = course.creation_date.split('T')[0].split('-').reverse().join('-')
+
   const { id, name } = course
   return (
     <div className='flex flex-col gap-4'>
       <div className='flex justify-between items-center'>
         <span className='text-muted'>#{id}</span>
-        <Link
-          href={`/home/courses/${id}`}
-          className='text-sm text-muted hover:text-accent duration-300'
-        >
-          View course
-        </Link>
+        <p className='text-xs p-1 text-muted'>{date}</p>
       </div>
-      <h2 className={`${roboto.className} font-extrabold text-xl text-primary`}>
+      <h2
+        className={`${roboto.className} font-extrabold text-xl text-primary tracking-tight h-20`}
+      >
         {name}
       </h2>
     </div>
