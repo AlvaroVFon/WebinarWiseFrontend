35370c705f2f8523c10bd24935f2ed574360379e
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-13 15:55:34 +0200

    fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,35 +7,28 @@ function Button({
   isLoading = false,
   spinnerWidth = 20,
   spinnerHeight = 20,
-  spinnerColor = '#3b82f6',
   width = 32,
   height = 12,
-  textColor = 'blue-500',
-  textHoverColor = 'blue-700',
-  bgColor = 'none',
-  bgHoverColor = 'none',
-  borderColor = 'blue-500',
-  borderHoverColor = 'blue-700',
+  padding = 2,
   disabled,
   disabledColor = 'muted',
   className,
 }) {
+  const style = disabled
+    ? `${className} flex items-center justify-center p-${padding} gap-3 w-${width} h-${height} text-muted rounded w-${width} h-${height} cursor-not-allowed`
+    : `${className} flex items-center justify-center p-${padding} gap-3 w-${width} h-${height} text-blue-500 hover:text-blue-700 border border-blue-500 hover:border-blue-700 duration-300 rounded`
   return (
     <button
       onClick={onClick}
       type={type}
-      className={
-        disabled
-          ? `${className} flex items-center justify-center gap-3 w-${width} h-${height} text-${disabledColor} border border-${disabledColor} rounded ${width} ${height} cursor-not-allowed`
-          : `${className} flex items-center justify-center gap-3 w-${width} h-${height} text-${textColor} hover:text-${textHoverColor} bg-${bgColor} hover:bg-${bgHoverColor} border border-${borderColor} hover:border-${borderHoverColor} duration-300 rounded`
-      }
+      className={style}
     >
       {isLoading ? loadingLabel : label}
       {isLoading && (
         <Spinner
           width={spinnerWidth}
           height={spinnerHeight}
-          color={spinnerColor}
+          color='#3b82f6'
         />
       )}
     </button>
