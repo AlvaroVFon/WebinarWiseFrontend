d61f896bc96003a612eaa4e1259081ae8aa5f771
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-05-21 08:27:35 +0200

    deploy fix

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { usePathname, useSearchParams, useParams } from 'next/navigation'
+import { Suspense } from 'react'
 function Breadcrumb({ categories, courses }) {
   const pathname = usePathname()
   const searchParams = useSearchParams()
@@ -16,13 +17,16 @@ function Breadcrumb({ categories, courses }) {
   const crumbs = pathname.split('/').filter(Boolean)
 
   return (
-    <p className='text-accent text-xl ml-14 mt-10'>
-      {crumbs.map((crumb, index) => {
-        if (crumb !== courseId) return <span key={index}> &#62; {crumb} </span>
-      })}
-      {categoryId && <span>&#62; {category?.name}</span>}
-      {courseName && <span>&#62; {courseName}</span>}
-    </p>
+    <Suspense>
+      <p className='text-accent text-xl ml-14 mt-10'>
+        {crumbs.map((crumb, index) => {
+          if (crumb !== courseId)
+            return <span key={index}> &#62; {crumb} </span>
+        })}
+        {categoryId && <span>&#62; {category?.name}</span>}
+        {courseName && <span>&#62; {courseName}</span>}
+      </p>
+    </Suspense>
   )
 }
 export default Breadcrumb
