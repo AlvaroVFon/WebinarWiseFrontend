e5172a40fbdb99f759ce3830d4317536745f2442
AlvaroVFon <130296209+AlvaroVFon@users.noreply.github.com>
2024-06-03 16:55:36 +0200

    CourseCard is now a Link

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -8,19 +8,18 @@ function CourseCard({ course, isPurchased, isLiked }) {
   const router = useRouter()
   return (
     <article className='bg-bgSecondary rounded-xl w-[285px] h-[414px] p-4 border border-muted hover:border-accent flex flex-col justify-between hover:scale-105 duration-300'>
-      <div
-        onClick={() => router.push(`/home/courses/${course.id}`)}
-        className='cursor-pointer'
-      >
+      <Link href={`/home/courses/${course.id}`}>
         <CourseCardHeader course={course} />
         <div className='flex flex-col items-end gap-4'>
-          <Link
-            href={`/home/courses?perPage=10&page=1&category=${course.category.id}`}
-            onClick={(e) => e.stopPropagation()}
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              router.push(`/home/courses?category=${course.category?.id}`)
+            }}
             className='border border-muted text-accentDarker p-1 rounded-md hover:bg-bgTertiary hover:text-accent cursor-pointer duration-300'
           >
             #{course.category?.name}
-          </Link>
+          </button>
           <Image
             src='https://placehold.jp/3d4070/ffffff/300x160.png'
             alt='Curso de React'
@@ -30,7 +29,7 @@ function CourseCard({ course, isPurchased, isLiked }) {
             priority
           />
         </div>
-      </div>
+      </Link>
       <CourseCardButtons
         course={course}
         isPurchased={isPurchased}
